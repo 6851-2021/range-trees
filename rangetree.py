9f42c2582f4995ac0d257c74a07647660c322920
izzykangaroo <40217319+izzykangaroo@users.noreply.github.com>
2021-03-07 19:26:01 -0500

    Create recursive vanilla range tree.

--- rangetree.py
+++ rangetree.py
@@ -1,31 +1,88 @@
+from bintrees import AVLTree as AVL
 import collections
 from typing import List, Tuple
-from sbbst import sbbst as SBBST
 
 Point = Tuple[float, ...]
 
+def augment_nodes(node):
+    dict_key = 'subtree_size'
+    subtree_nodes = 'subtree_nodes'
+    coord = 'coord'
+    
+    if node is None:
+        return False
+    
+    if not node.left and not node.right:
+        node.value[dict_key] = 1
+        node.value[subtree_nodes] = [node.value[coord]]
+        return
+    
+    augment_nodes(node.left)
+    augment_nodes(node.right)
+    
+    if node.left:
+        left_subtree = node.left.value[dict_key]
+        left_nodes = node.left.value[subtree_nodes]
+    else:
+        left_subtree = 0
+        left_nodes = []
+    if node.right:
+        right_subtree = node.right.value[dict_key]
+        right_nodes = node.right.value[subtree_nodes]
+    else:
+        right_subtree = 0
+        right_nodes = []
+    
+    node.value[dict_key] = left_subtree + right_subtree + 1
+    node.value[subtree_nodes] = left_nodes + [node.value[coord]] + right_nodes
+    return node
+
+def traverse(node):
+    if not node:
+        yield None
+    if node.left:
+        yield from traverse(node.left)
+    yield node
+    if node.right:
+        yield from traverse(node.right)
 
 class StaticVanillaRangeTree:
-    def __init__(self, points: List[Point], dim: int = 0):
-        """Recursively construct a range tree on k-dimensional data."""
-        assert len(points[0]) >= 1
-        #assert len(points[0]) == dim
-        if dim == 1:
-            return SBBST([point[0] for point in points])
-        first_coords = [point[dim - 1] for point in points]
-        tree = SBBST(first_coords)
-
+    def __init__(self, points: List[Point]):
         
-        # for point in points:
-        #     #fix this; needs to insert all the nodes in the subtree
-        #     tree.insert(StaticVanillaRangeTree(points, dim - 1), point[dim - 1])
+        assert len(points) > 0
 
-    def query(self, p1: Point, p2: Point) -> List[Point]:
-        """Returns points in the k-dimensional box formed by p1 and p2."""
-        # TODO: Return a generator instead of a list so that we only
-        # read points we need.
-        pass
+        self.tree = self.construct_tree(points)
+        
+    def print_tree(self, root):
+        for node in traverse(root):
+            print('key:', node.key)
+            print('val:', node.value)
+        print("-------------------")
+    
+    def construct_tree(self, points):
+        assert len(points) > 0
+        
+        dim = len(points[0])
+        
+        if dim == 1:
+            tree = AVL()
+            for point in points:
+                x, rest = point[0], point[1:]
+                tree.insert(x, {'coord': point})
+            augment_nodes(tree._root)
+            return tree
+        else:
+            tree = AVL()
+            for point in points:
+                x, rest = point[0], point[1:]
+                tree.insert(x, {'coord': point})
+            augment_nodes(tree._root)
 
+            self.print_tree(tree._root)
 
-if __name__ == '__main__':
-    pass
+            for node in traverse(tree._root):
+                subtree_nodes = [coord[1:] for coord in node.value['subtree_nodes']]
+                recursive_range_tree = StaticVanillaRangeTree(subtree_nodes)
+                node.value['sub_range_tree'] = recursive_range_tree
+                self.print_tree(recursive_range_tree.tree._root)
+            return tree
